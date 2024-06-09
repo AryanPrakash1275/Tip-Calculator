0be3c2d5f9264e916423042d90dd1985c3d9c89d
Aryan Prakash <aryanprakash1275@gmail.com>
2024-06-09 15:01:19 +0530

    add inputs and maths

--- Tip-Calculator.py
+++ Tip-Calculator.py
@@ -1,2 +1,13 @@
 print("Welcome to Tip-Calculator\n")
 print("Created by Aryan")
+
+total_bill = float(input("What was the total bill?\n"))
+tip_percent = float(input("How much would you like to tip? Please input in percentage.\n"))
+number_of_people = int(input("How many people to split the bill?\n"))
+
+#maths
+calc1TIP = float((total_bill * tip_percent)/100)
+calc2TB = float(total_bill + calc1TIP)
+calc3FINAL = float(calc2TB/number_of_people)
+
+
